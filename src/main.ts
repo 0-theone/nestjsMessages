5ea806bda4b9156524a64765a7660e5887850f53
Raissa Hohenester <raissa.developer@gmail.com>
2022-12-05 17:16:38 +0100

    Add validation Pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { MessagesModule } from './messages/messages.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(MessagesModule);
+  //Validate requests
+  app.useGlobalPipes(
+    new ValidationPipe()
+  );
   await app.listen(3000);
 }
 bootstrap();
