6e45a5bf676cc9dfffd7f980a13ba3141825a737
Raissa Hohenester <raissa.developer@gmail.com>
2022-12-07 12:02:48 +0100

    Add first draft of messages service

---
+++ src/messages/messages.service.ts
@@ -0,0 +1,24 @@
+import { MessagesRepository } from './messages.repository';
+
+export class MessagesServices {
+    messagesRepo: MessagesRepository;
+
+    constructor() {
+        // Servie is creating its own dependencies
+        // Dont do this on real apps - temporary!
+        this.messagesRepo = new MessagesRepository();
+    }
+
+    findOne(id: string) {
+        return this.messagesRepo.findOne(id);
+    }
+
+    findAll() {
+        return this.messagesRepo.findAll();
+    }
+
+    create(content: string) {
+        return this.messagesRepo.create(content);
+    }
+
+}
